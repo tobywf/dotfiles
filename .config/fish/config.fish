fe591e67b56750109d38ad745e52d307b839730e
Toby Fleming <toby.fleming@kogan.com.au>
2017-06-11 16:06:20 +1000

    Updated settings 2017

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,41 +1,37 @@
 set fish_greeting
 
-set -gx LC_ALL en_GB.UTF-8
-set -gx LC_CTYPE en_GB.UTF-8
+set -x LC_ALL en_US.UTF-8
+set -x LC_CTYPE en_US.UTF-8
 
 # fish will show a prompt, don't mess with my theme
-set -gx VIRTUAL_ENV_DISABLE_PROMPT 1
+set -x VIRTUAL_ENV_DISABLE_PROMPT 1
 
-# don't allow global pip by accident when no virtualenv is sourced
-set -gx PIP_REQUIRE_VIRTUALENV true
+# don't allow pip by accident outside a virtualenv
+set -x PIP_REQUIRE_VIRTUALENV true
 
-# allow global pip if necessary when no virtualenv is sourced
-function gpip
-    begin
-        set -lx PIP_REQUIRE_VIRTUALENV
-        sudo -H /usr/local/bin/pip3 $argv
-    end
+# allow pip on purpose outside a virtualenv
+function gpip3
+    env PIP_REQUIRE_VIRTUALENV="" sudo -H /usr/local/bin/pip3 $argv
 end
 
 function gpip2
-    begin
-        set -lx PIP_REQUIRE_VIRTUALENV
-        sudo -H /usr/local/bin/pip $argv
-    end
+    env PIP_REQUIRE_VIRTUALENV="" sudo -H /usr/local/bin/pip2 $argv
 end
 
 # hacks to use XDG config location for easier backup
 alias tmux "tmux -2f $HOME/.config/tmux/config"
 
 # less, don't save any history
-set -gx LESSHISTFILE /dev/null
+set -x LESSHISTFILE /dev/null
 
-set -gx DOTFILES "$HOME/.local/share/dotfiles/"
+set -x DOTFILES "$HOME/.local/share/dotfiles/"
 
 # https://developer.atlassian.com/blog/2016/02/best-way-to-store-dotfiles-git-bare-repo/
 alias dot "git --git-dir=$DOTFILES --work-tree=$HOME"
 
-# homebrew
-set -gx HOMEBREW_CASK_OPTS "--appdir=$HOME/Applications"
+set -x PATH "$HOME/.local/share/bin" $PATH
 
-set -gx PATH "$HOME/.local/share/bin/" $PATH
+if command -v docker-machine > /dev/null
+    eval (docker-machine env --shell fish)
+    # set -x DOCKER_IP (docker-machine ip)
+end
